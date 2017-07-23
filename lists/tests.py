e5119bda2037596fc3362781f0be5d9380ff4628
Asher <asherdiamant@users.noreply.github.com>
2017-07-23 17:29:16 -0400

    Still can't handle more than one item in list

--- lists/tests.py
+++ lists/tests.py
@@ -4,16 +4,25 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+import re
 
 # Create your tests here.
+
+
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve ('/')
-		self.assertEqual(found.func, home_page)
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(self.remove_csrf(html_code1), self.remove_csrf(html_code2))
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve ('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
