beb04625788aa75054c99e041f6c2a8323390c0b
Asher <asherdiamant@users.noreply.github.com>
2017-07-27 09:45:31 -0400

    Fixed typo in item validation test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -24,8 +24,8 @@ class ItemValidationTests(FunctionalTest):
 		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
 		# She receives a similar warning on the list page
-		self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
-		
+		self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text, 
+				"You can't have an empty list item"))
 		# And she can correct it by filling some text in
 		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
 		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
