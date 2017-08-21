f5bc098c15d4e933f50dcd86ea9f9b1e538cf8c6
Asher <asherdiamant@users.noreply.github.com>
2017-08-21 16:40:31 -0400

    lists can have owners, which are saved on creation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 from lists.models import Item, List
 
@@ -16,7 +19,6 @@ class ItemModelTest(TestCase):
         item.list = list_
         item.save()
         self.assertIn(item, list_.item_set.all())
-
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -39,8 +41,7 @@ class ItemModelTest(TestCase):
         item = Item(list=list2, text="bla")
         item.full_clean() # Should not raise
 
-
-    def test_list_oredering(self):
+    def test_list_ordering(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='i1')
         item2 = Item.objects.create(list=list1, text='item 2')
@@ -51,8 +52,17 @@ class ItemModelTest(TestCase):
         item = Item(text='some_text')
         self.assertEqual(str(item), 'some_text')
 
+
 class ListModelTest(TestCase):
 
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+    def test_lists_can_have_owners(self):
+        user = User.objects.create(email='a@b.com')
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+
+    def test_list_owner_is_optional(self):
+        List.objects.create() # Should not raise
