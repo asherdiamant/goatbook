a515f09d423952af3dea56546fad1a165148e568
Asher <asherdiamant@users.noreply.github.com>
2017-08-24 12:23:38 -0400

    Moved wait to be a module function not in a class

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -12,6 +12,21 @@ from .management.commands.create_session import create_pre_authenticated_session
 
 MAX_WAIT = 10
 
+
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -24,17 +39,7 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except(AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
+
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
